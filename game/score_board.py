7c116f5ddde28af14f3c4c31cd7cde239caa2773
Sebastian Doiber <sebastian.doiber@stud.fh-campuswien.ac.at>
2019-12-02 16:49:20 +0100

    sidenotes for the point system | "implementation" of winner method

--- game/score_board.py
+++ game/score_board.py
@@ -2,8 +2,20 @@ class ScoreBoard:
     """
     total_score = Liste mit allen round scores/Endergebnissen aller Runden.
     round_score = Anzahl angesagter Stiche/gemachter Stiche einer spezifischen runde + ausgespielter Karten.
+
+    +20pkt f. richtige Vorhersage
+    -10pkt f. jede abweichende Vorhersage
+    +10pkt f. jeden gemachtden Stich
+
     """
 
     def __init__(self):
         self.total_score = list()  # liste mit allen round scores/endergebnissen aller runden
         self.round_score = list()  # anzahl angesagter sticht/gemachter stiche einer spezifischen runde
+
+    def winner(self, player):
+        """
+        soll am ende des spiels ausgeben wer gewonnen hat
+        :return:
+        """
+        print('congratz', player, 'you won')
