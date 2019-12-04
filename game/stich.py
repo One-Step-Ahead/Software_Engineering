280a276b3107224389199d714ffc240d21c84d71
Johannes <joderpro28@gmail.com>
2019-12-04 12:48:28 +0100

    refactored Stich and rules.py

--- game/stich.py
+++ game/stich.py
@@ -1,8 +1,7 @@
 from collections import deque
 
-from game.cards import input_card
-from game.game_board import Player, Card
-from game.rules import check_wizzad_win, check_if_playable
+from game.cards import input_card, ColoredCard
+from game.game_board import Player, Card, SpecialCard
 
 
 class Stich:
@@ -11,17 +10,65 @@ class Stich:
         self.cards_in_play = list()
         self.winner = Player
 
-    def get_player(player_number: int, player_q: list):
+    def farbzwang_check(self, card_about_to_be_played: Card) -> bool:
+        """
+        :return True if card matches playable "Farbzwang" conditions
+        """
+        first_played_card = self.cards_in_play[0]
+
+        if isinstance(card_about_to_be_played, SpecialCard):
+            return True
+        elif isinstance(card_about_to_be_played, ColoredCard) and isinstance(first_played_card, ColoredCard):
+            if card_about_to_be_played.cardColor == first_played_card.cardColor:
+                return True
+        else:
+            return False
+
+    def cant_serve(self, player: Player):
+        """
+        If the player has no cards that allow him to serve the color on the board :return True.
+        """
+        for i in player.hand:
+            if self.farbzwang_check(i):
+                return False
+        return True
+
+    def check_wizzad_win(self):
+        for i in self.cards_in_play:
+            if isinstance(i, SpecialCard):
+                if i.cardType == 'z':
+                    return True
+            else:
+                return False
+
+    def check_if_playable(self, chosen_card: Card, player: Player) -> bool:
+        """
+        Checks if the card you are about to play is legit.
+        :param player: The Player that wants to play the chosen_card.
+        :param chosen_card: Card you are about to play.
+        :return:
+        """
+        if len(self.cards_in_play) == 0:
+            return True
+        elif type(chosen_card) is type(SpecialCard):
+            return True
+        elif self.cant_serve(player):
+            return True
+        elif self.farbzwang_check(chosen_card):
+            return True
+        else:
+            return False
+
+    def get_player(self, player_number: int, player_q: list):
         return player_q[player_number]
 
-    def check_stich_winner(self, cards_in_play: list, player_q: deque) -> Player:
+    def check_stich_winner(self) -> type(Player):
         """
-
-        :param cards_in_play:
         :return: Player that won the Stich
         """
-        if check_wizzad_win(cards_in_play):  # todo Bitte mit Stich implementieren (returnt nix)
-            return Player
+
+        if self.check_wizzad_win():
+            return self.winner
 
     def play(self, player_queue: deque):
         """
@@ -34,9 +81,9 @@ class Stich:
             print("It is now the turn of Player:", i.id)
             while allowed:
                 chosen_card = input_card(i.hand)
-                if check_if_playable(chosen_card, self.cards_in_play, i.hand):
+                if self.check_if_playable(chosen_card, i):
                     allowed = False
                 else:
                     print('You are not allowed to play this card right now!')
-            i.play_card(chosen_card, i.hand)
-        self.check_stich_winner(self.cards_in_play, player_queue)
+            i.play_card(chosen_card, player_queue)
+        self.check_stich_winner()
