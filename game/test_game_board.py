0846b43c9180b77e9221a9213853fcd04356b067
Sebastian Doiber <sebastian.doiber@stud.fh-campuswien.ac.at>
2019-12-28 17:46:25 +0100

    Added test_game_board.py

---
+++ game/test_game_board.py
@@ -0,0 +1,15 @@
+import unittest
+
+from game.cards import ColoredCard
+from game.game_board import *
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.g = GameBoard(4)
+
+    def test_set_stich_queue(self):
+        stich1 = Stich(self.g.player_queue, ColoredCard(1, 'red'))
+        stich1.winner = self.g.player_queue[1]
+        self.assertEqual(self.g.player_queue[1], self.g.set_stich_queue(2)[0])
