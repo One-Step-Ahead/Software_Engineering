39638e9cc149519c25a451bef9950f183af8103d
Johannes Schwinger <johannes.schwinger@gmx.at>
2019-12-29 12:22:43 +0100

    implemented new testcase in test_round.py

--- game/test_round.py
+++ game/test_round.py
@@ -15,7 +15,7 @@ class MyTestCase(unittest.TestCase):
         del self.g
         del self.players
 
-    def test_score(self):  # todo find (test)case name
+    def test_score_round_1(self):  # todo find (test)case name
         round = Round(1)
         stich1 = Stich(self.g.player_queue, ColoredCard(0, 'red'))
         round.stiche.append(stich1)
@@ -30,4 +30,27 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.g.players[0].score, 30)
         self.assertEqual(self.g.players[1].score, 20)
         self.assertEqual(self.g.players[2].score, 20)
+        self.assertEqual(self.g.players[3].score, -10)
+
+    def test_score_round_3(self):
+        round = Round(3)
+        stich1 = Stich(self.g.player_queue, ColoredCard(0, 'red'))
+        stich2 = Stich(self.g.player_queue, ColoredCard(0, 'red'))
+        stich3 = Stich(self.g.player_queue, ColoredCard(0, 'red'))
+        round.stiche.append(stich1)
+        round.stiche.append(stich2)
+        round.stiche.append(stich3)
+        stich1.winner = self.g.players[0]
+        stich2.winner = self.g.players[1]
+        stich3.winner = self.g.players[0]
+        prediction = {self.players[0]: 2,
+                      self.players[1]: 3,
+                      self.players[2]: 0,
+                      self.players[3]: 1}
+        round.predictions = prediction
+
+        round.calculate_score(self.g.players)
+        self.assertEqual(self.g.players[0].score, 40)
+        self.assertEqual(self.g.players[1].score, -20)
+        self.assertEqual(self.g.players[2].score, 30)
         self.assertEqual(self.g.players[3].score, -10)
