a1055cd9087e3be0081c4227b2082542ca53fc91
Johannes Schwinger <joderpro28@gmail.com>
2020-01-05 14:06:01 +0100

    added testcases

--- test/test_stich.py
+++ test/test_stich.py
@@ -6,14 +6,13 @@ from game.stich import Stich
 
 
 # noinspection DuplicatedCode
-class TestStichWinner(TestCase):
+class TestStichWinnerAtutRed(TestCase):
     def setUp(self) -> None:
-        self.g = GameBoard(4, True)
+        self.g = GameBoard(4, False)
         self.g.new_round()
-
-    def test_single_atutwin(self):
         self.s = Stich(self.g.player_queue, ColoredCard(1, 'red'))
 
+    def test_single_atutwin(self):
         self.s.cards_in_play.append(ColoredCard(5, 'green'))
         self.s.cards_in_play.append(ColoredCard(6, 'green'))
         self.s.cards_in_play.append(ColoredCard(10, 'green'))
@@ -22,8 +21,6 @@ class TestStichWinner(TestCase):
         self.assertEqual(self.s.check_stich_winner(), self.s.player_q[3])
 
     def test_colorwin(self):
-        self.s = Stich(self.g.player_queue, ColoredCard(1, 'red'))
-
         self.s.cards_in_play.append(ColoredCard(5, 'green'))
         self.s.cards_in_play.append(ColoredCard(6, 'green'))
         self.s.cards_in_play.append(ColoredCard(10, 'green'))
@@ -32,8 +29,6 @@ class TestStichWinner(TestCase):
         self.assertEqual(self.s.check_stich_winner(), self.s.player_q[3])
 
     def test_wizzard_win(self):
-        self.s = Stich(self.g.player_queue, ColoredCard(1, 'red'))
-
         self.s.cards_in_play.append(SpecialCard("z"))
         self.s.cards_in_play.append(ColoredCard(6, 'green'))
         self.s.cards_in_play.append(ColoredCard(10, 'green'))
@@ -42,8 +37,6 @@ class TestStichWinner(TestCase):
         self.assertEqual(self.s.check_stich_winner(), self.s.player_q[0])
 
     def test_narr_only_win(self):
-        self.s = Stich(self.g.player_queue, ColoredCard(1, 'red'))
-
         self.s.cards_in_play.append(SpecialCard("n"))
         self.s.cards_in_play.append(SpecialCard("n"))
         self.s.cards_in_play.append(SpecialCard("n"))
@@ -52,8 +45,6 @@ class TestStichWinner(TestCase):
         self.assertEqual(self.s.check_stich_winner(), self.s.player_q[0])
 
     def test_multiple_atut_win(self):
-        self.s = Stich(self.g.player_queue, ColoredCard(1, 'red'))
-
         self.s.cards_in_play.append(ColoredCard(6, 'red'))
         self.s.cards_in_play.append(ColoredCard(10, 'red'))
         self.s.cards_in_play.append(ColoredCard(13, 'red'))
@@ -62,8 +53,6 @@ class TestStichWinner(TestCase):
         self.assertEqual(self.s.check_stich_winner(), self.s.player_q[2])
 
     def test_dominant_color_win(self):
-        self.s = Stich(self.g.player_queue, ColoredCard(1, 'red'))
-
         self.s.cards_in_play.append(ColoredCard(12, 'green'))
         self.s.cards_in_play.append(ColoredCard(10, 'green'))
         self.s.cards_in_play.append(ColoredCard(13, 'yellow'))
@@ -71,9 +60,55 @@ class TestStichWinner(TestCase):
 
         self.assertEqual(self.s.check_stich_winner(), self.s.player_q[0])
 
-    def test_none_atut(self):
+
+# noinspection DuplicatedCode
+class TestStichWinnerAtutNone(TestCase):
+    def setUp(self) -> None:
+        self.g = GameBoard(4, False)
+        self.g.new_round()
         self.s = Stich(self.g.player_queue, None)
 
+    def test_single_atutwin(self):
+        self.s.cards_in_play.append(ColoredCard(5, 'green'))
+        self.s.cards_in_play.append(ColoredCard(6, 'green'))
+        self.s.cards_in_play.append(ColoredCard(10, 'green'))
+        self.s.cards_in_play.append(ColoredCard(5, 'red'))
+
+        self.assertEqual(self.s.check_stich_winner(), self.s.player_q[2])
+
+    def test_colorwin(self):
+        self.s.cards_in_play.append(ColoredCard(5, 'green'))
+        self.s.cards_in_play.append(ColoredCard(6, 'green'))
+        self.s.cards_in_play.append(ColoredCard(10, 'green'))
+        self.s.cards_in_play.append(ColoredCard(13, 'green'))
+
+        self.assertEqual(self.s.check_stich_winner(), self.s.player_q[3])
+
+    def test_wizzard_win(self):
+        self.s.cards_in_play.append(SpecialCard("z"))
+        self.s.cards_in_play.append(ColoredCard(6, 'green'))
+        self.s.cards_in_play.append(ColoredCard(10, 'green'))
+        self.s.cards_in_play.append(ColoredCard(13, 'green'))
+
+        self.assertEqual(self.s.check_stich_winner(), self.s.player_q[0])
+
+    def test_narr_only_win(self):
+        self.s.cards_in_play.append(SpecialCard("n"))
+        self.s.cards_in_play.append(SpecialCard("n"))
+        self.s.cards_in_play.append(SpecialCard("n"))
+        self.s.cards_in_play.append(SpecialCard("n"))
+
+        self.assertEqual(self.s.check_stich_winner(), self.s.player_q[0])
+
+    def test_multiple_atut_win(self):
+        self.s.cards_in_play.append(ColoredCard(6, 'red'))
+        self.s.cards_in_play.append(ColoredCard(10, 'red'))
+        self.s.cards_in_play.append(ColoredCard(13, 'red'))
+        self.s.cards_in_play.append(SpecialCard('n'))
+
+        self.assertEqual(self.s.check_stich_winner(), self.s.player_q[2])
+
+    def test_dominant_color_win(self):
         self.s.cards_in_play.append(ColoredCard(12, 'green'))
         self.s.cards_in_play.append(ColoredCard(10, 'green'))
         self.s.cards_in_play.append(ColoredCard(13, 'yellow'))
